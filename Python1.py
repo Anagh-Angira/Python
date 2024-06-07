f123f1cee7bf88726b6880d92bbabbc12c87af4e
Anagh-Angira <2041001057.anaghangira@gmail.com>
2024-06-07 08:49:42 +0530

    Initial commit

---
+++ Python1.py
@@ -0,0 +1,44 @@
+print("Hello..World")
+print("Hello..Aman")
+
+# Variables in Python
+
+print("This is The Python Demo")
+
+a = 3
+print(a)
+b = 55
+print(b)
+a = 6.6
+print(a) #This is Possible in Python
+c = True
+print(c)
+
+d = "Harry" #String is Sequence of Charecter
+print(d)
+
+#In Python all datatypes are Object
+e = None #Nothing
+print(e)
+
+
+#TypeCasting
+f = "5"
+print(int(f)+1)
+# print(f + 1) Error String cannot concat with Integer
+g = 5
+print(g)
+
+h = 7
+print(type(a))
+
+n = 6
+
+m = float(n)
+print(m)
+print(type(m))
+
+x = 5
+print(type(x))
+y = str(x)
+print(type(y)," ", type(x))
---
+++ Python2.py
@@ -0,0 +1,42 @@
+#Arithmatic Operator
+num1 = 345
+num2 = 4
+print("num1 + num2 ",num1+num2)
+print("num1 - num2 ",num1-num2)
+print("num1 * num2 ",num1*num2)
+print("num1 / num2 ",num1/num2)
+print("num1 % num2 ",num1%num2)
+print("num1 // num2 ",num1//num2) #floor division
+
+num3 = 2
+num4 = 3
+print("num1 ** num2 ",num1**num2) #Power
+
+#Assignment Operator
+
+a = 4
+# a += 2
+a -= 2
+
+print(a)
+
+#Comparision
+
+x = 3
+y = 4
+z = 3
+print(y > x)
+print(x > y)
+print(x != y)
+print(x == y)
+print(x == z)
+
+# "=" Assignment Op "==" Comparision Op
+
+#Logical Op
+print(x == z and x < y)
+print(x == z or x < y)
+print(not(False))
+print(not(True))
+
+
